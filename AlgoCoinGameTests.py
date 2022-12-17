1dbea8e345ba7dd8fb5d89518fbc5874d2010a53
Veasna Ling <v_ling@u.pacific.edu>
2022-12-16 20:03:14 -0800

    Increase test case size and add more test cases.

--- AlgoCoinGameTests.py
+++ AlgoCoinGameTests.py
@@ -3,6 +3,7 @@ import AlgoCoinGame as CG
 import time
 
 
+
 class MyTestCase(unittest.TestCase):
     testcase1 = [1, 5, 10, 25]
 
@@ -12,71 +13,6 @@ class MyTestCase(unittest.TestCase):
 
     testcase4 = [10, 5, 10, 25, 10, 5, 10, 25, 10, 5, 10, 5, 1, 5, 10, 5, 1, 5, 10, 25, 1, 5, 10, 25,
                  1, 5, 10, 25]
-
-    testcase5 = [1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 1, 5, 10, 25, 1]
-
-    testcase6 = [1, 5, 10, 25, 1, 5, 1, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 10, 5, 10, 25, 1, 5, 10, 5, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 1, 25, 1, 5, 10, 1,
-                 25, 5, 10, 5, 1, 5, 10, 25, 25, 1, 5, 10, 1, 25, 1, 5, 10, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 5, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 6,
-                 25, 5, 10, 25, 1, 5, 10, 25, 25, 1, 5, 10, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 5]
-
-    testcase7 = [1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 10, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 1,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 6,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 5,
-                 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 10, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 6,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 25, 1, 5, 10, 5,
-                 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 10, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 6,
-                 25, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 5,
-                 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 10, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 1,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 6,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 5,
-                 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 10, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 6,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 25, 1, 5, 10, 5,
-                 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 10, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 6,
-                 25, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 5,
-                 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 10, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 1,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 6,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 5,
-                 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 10, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 6,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 25, 1, 5, 10, 5,
-                 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 10, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 1,
-                 25, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25,
-                 5, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 6,
-                 25, 5, 10, 25, 1, 5, 10, 10, 5, 1, 5, 25, 1, 5, 10, 25, 1, 5, 10, 25, 1, 5, 10, 5]
 
     def test_case1(self):
         n = len(self.testcase1)
@@ -179,64 +115,119 @@ class MyTestCase(unittest.TestCase):
         print("\n===============================================")
 
     def test_case5(self):
-        n = len(self.testcase5)
+        testcase5 = []
+        for i in range(1001):
+            if i % 2 == 2:
+                testcase5.append(25)
+            elif i % 3 == 1:
+                testcase5.append(10)
+            elif i % 2:
+                testcase5.append(1)
+            else:
+                testcase5.append(5)
+
+        n = len(testcase5)
         print(f"test_case 5 \nsize: {n}")
         start_time = time.time()
-        brute1 = CG.BruteCoinGame()
-        result1 = brute1.brute_coin_game(self.testcase5, 0, n - 1)
-        end_time = time.time() - start_time
-        print("brute 5 time: %s" % end_time)
-        self.assertEqual(78, result1)
-
-        start_time = time.time()
-        greed1 = CG.GreedyCoinGame(self.testcase5, n)
+        greed1 = CG.GreedyCoinGame(testcase5, n)
         result2 = greed1.greed_function()
         end_time = time.time() - start_time
         print("greed 5 time: %s" % end_time)
-        self.assertEqual(78, result2)
+        self.assertEqual(2012, result2)
 
         start_time = time.time()
         dp1 = CG.DPCoinGame()
-        result3 = dp1.dp_function(self.testcase5, n)
+        result3 = dp1.dp_function(testcase5, n)
         end_time = time.time() - start_time
         print("dp 5 time: %s" % end_time)
-        self.assertEqual(78, result3)
+        self.assertEqual(2012, result3)
         print("\n===============================================")
 
     def test_case6(self):
-        n = len(self.testcase6)
+        testcase6 = []
+        for i in range(2001):
+            if i % 2 == 2:
+                testcase6.append(25)
+            elif i % 3 == 1:
+                testcase6.append(10)
+            elif i % 2:
+                testcase6.append(1)
+            else:
+                testcase6.append(5)
+
+        n = len(testcase6)
         print(f"test_case 6 \nsize: {n}")
         start_time = time.time()
-        greed1 = CG.GreedyCoinGame(self.testcase6, n)
+        greed1 = CG.GreedyCoinGame(testcase6, n)
         result2 = greed1.greed_function()
         end_time = time.time() - start_time
         print("greed 6 time: %s" % end_time)
-        self.assertEqual(733, result2)
+        self.assertEqual(4006, result2)
 
         start_time = time.time()
         dp1 = CG.DPCoinGame()
-        result3 = dp1.dp_function(self.testcase6, n)
+        result3 = dp1.dp_function(testcase6, n)
         end_time = time.time() - start_time
         print("dp 6 time: %s" % end_time)
-        self.assertEqual(895, result3)
+        self.assertEqual(4006, result3)
         print("\n===============================================")
 
     def test_case7(self):
-        n = len(self.testcase7)
+        testcase7 = []
+        for i in range(4001):
+            if i % 2 == 2:
+                testcase7.append(5)
+            elif i % 3 == 1:
+                testcase7.append(10)
+            elif i % 2:
+                testcase7.append(1)
+            else:
+                testcase7.append(25)
+
+        n = len(testcase7)
         print(f"test_case 7 \nsize: {n}")
         start_time = time.time()
-        greed1 = CG.GreedyCoinGame(self.testcase7, n)
+        greed1 = CG.GreedyCoinGame(testcase7, n)
         result2 = greed1.greed_function()
         end_time = time.time() - start_time
         print("greed 7 time: %s" % end_time)
-        self.assertEqual(6060, result2)
+        self.assertEqual(8028, result2)
 
         start_time = time.time()
         dp1 = CG.DPCoinGame()
-        result3 = dp1.dp_function(self.testcase7, n)
+        result3 = dp1.dp_function(testcase7, n)
         end_time = time.time() - start_time
         print("dp 7 time: %s" % end_time)
-        self.assertEqual(8709, result3)
+        self.assertEqual(8028, result3)
+        print("\n===============================================")
+
+    def test_case8(self):
+        testcase8 = []
+        for i in range(8001):
+            if i % 2 == 2:
+                testcase8.append(5)
+            elif i % 3 == 1:
+                testcase8.append(10)
+            elif i % 2:
+                testcase8.append(1)
+            else:
+                testcase8.append(25)
+        # print(testcase8)
+        n = len(testcase8)
+        print(f"test_case 8 \nsize: {n}")
+        start_time = time.time()
+        greed1 = CG.GreedyCoinGame(testcase8, n)
+        result2 = greed1.greed_function()
+        end_time = time.time() - start_time
+        print("greed 8 time: %s" % end_time)
+        self.assertEqual(16031, result2)
+
+        start_time = time.time()
+        dp1 = CG.DPCoinGame()
+        result3 = dp1.dp_function(testcase8, n)
+        end_time = time.time() - start_time
+        print("dp 8 time: %s" % end_time)
+        self.assertEqual(16031, result3)
         print("\n===============================================")
 
 
